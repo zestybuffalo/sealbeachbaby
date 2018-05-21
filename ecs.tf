1a033f8f8325f8758923339633de3414f1bc910b
zestybuffalo <37966436+zestybuffalo@users.noreply.github.com>
2018-05-21 11:37:53 -0700

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,12 +1,12 @@
-resource "aws_ecs_cluster" "overhead-cluster" {
-    name = "overhead-cluster"
+resource "aws_ecs_cluster" "name" {
+    name = "clustername"
 }
-resource "aws_launch_configuration" "ecs-overhead-launchconfig" {
-  name_prefix          = "ecs-overhead-launchconfig"
+resource "aws_launch_configuration" "ecs-clustername-launchconfig" {
+  name_prefix          = "ecs-clustername-launchconfig"
   image_id             = "${lookup(var.ECS_AMIS, var.AWS_REGION)}"
   key_name             = "${aws_key_pair.mykeypair.key_name}"
   instance_type        = "${var.ECS_INSTANCE_TYPE}"  
-  security_groups      = ["${aws_security_group.surflineusa-securitygroup.id}"]
-  user_data            = "#!/bin/bash\necho 'ECS_CLUSTER=overhead-cluster' > /etc/ecs/ecs.config\nstart ecs"
+  security_groups      = ["${aws_security_group.sgname-securitygroup.id}"]
+  user_data            = "#!/bin/bash\necho 'ECS_CLUSTER=cluster_name-cluster' > /etc/ecs/ecs.config\nstart ecs"
   lifecycle              { create_before_destroy = true }
 }
